990b4c2b986f6df7974c30586635075f4ca95d21
rindra josia <rrindrajosia@gmail.com>
2020-10-26 23:21:48 +0300

    add link to index js file

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './css/style.css';
+import loading from './view/loading';
+import formEvent from './event/form';
+import modalError from './view/error';
+
+modalError();
+loading();
+formEvent();
